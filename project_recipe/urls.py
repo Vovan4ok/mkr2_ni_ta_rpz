942ddb2817235ce31a45647534022fde7ba8347c
Volodymyr Zganyayko <bosik_ck@ukr.net>
2023-05-25 09:31:18 +0300

    2nd task

--- project_recipe/urls.py
+++ project_recipe/urls.py
@@ -6,4 +6,6 @@ from recipe import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('main/', views.main),
+    path('category_list/', views.category_list),
+    path('', views.main, name='main')
 ]
--- recipe/views.py
+++ recipe/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Recipe, Category
+from django.db.models import Count
 
 
 def main(request):
@@ -7,3 +8,12 @@ def main(request):
     return render(request, 'main.html', {'recipes': recipes})
 
 
+def category_list(request):
+    categories = Category.objects.all()
+    category_counts = []
+    for category in categories:
+        category_counts.append({
+            'name': category.name,
+            'count': category.categories.count()
+        })
+    return render(request, 'category_list.html', {'category_counts': category_counts})
