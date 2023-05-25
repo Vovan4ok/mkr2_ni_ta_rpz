5598bd157bedc8607fd170674f912cd89f6a21b4
Volodymyr Zganyayko <bosik_ck@ukr.net>
2023-05-25 09:15:45 +0300

    1st task

--- recipe/views.py
+++ recipe/views.py
@@ -1,1 +1,9 @@
+from django.shortcuts import render
+from .models import Recipe, Category
+
+
+def main(request):
+    recipes = Recipe.objects.order_by('-created_at')[:5]
+    return render(request, 'main.html', {'recipes': recipes})
+
 
